520ab4a41d0a09ca686a716c44c7f7ce30e8bfdc
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2021-09-25 16:37:05 -0400

    implemented responsive navbar
    added in the final bits for a responsive navbar

--- static/index.js
+++ static/index.js
@@ -1,1 +1,16 @@
 //next thing to do is add in a function for clicking on the hamburger icon to pop the navbar up
+
+
+let mainNavBar = document.getElementById('js-main-menu')
+let navBarToggle = document.getElementById('js-nav-toggle')
+
+
+navBarToggle.addEventListener('click', function(){
+        if(mainNavBar.className === 'active'){
+                mainNavBar.className = 'main-nav'
+        } else {
+                mainNavBar.className = 'active'
+        }
+        console.log(mainNavBar.className)
+});
+
