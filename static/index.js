66a64234a9b36b4bf042c2ee60f9b0926356632c
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2023-10-29 13:21:38 -0400

    Fixed NavBar issue on mobile
    added isnavbar open flag to change margin top of the main section of content of the page

--- static/index.js
+++ static/index.js
@@ -3,14 +3,25 @@
 
 let mainNavBar = document.getElementById('js-main-menu')
 let navBarToggle = document.getElementById('js-nav-toggle')
-
+let mainSection = document.querySelector("section") // Selecting the section
+let isNavbarOpen = false
 
 navBarToggle.addEventListener('click', function(){
+        
+
         if(mainNavBar.className === 'active'){
                 mainNavBar.className = 'main-nav'
+                isNavbarOpen = true
         } else {
                 mainNavBar.className = 'active'
+                isNavbarOpen = false
         }
         console.log(mainNavBar.className)
+
+        if(isNavbarOpen){
+                mainSection.style.marginTop = '0px'
+        } else {
+                mainSection.style.marginTop = '70px'
+        }
 });
 
